bbd241435fcecfaa089f0699d3b860d6985109d4
Rishi SHeth <sheth19@purdue.edu>
2020-06-09 23:17:17 -0400

    Update converter-test.js

--- test/converter-test.js
+++ test/converter-test.js
@@ -1,22 +1,22 @@
 const fs = require('fs');
 const path = require("path");
 var assert = require('assert');
-var convert= require('csvandjsonconverter');
+var convert = require('../lib/converter');
 
 describe('Converter', function() {
-    convert.CSVtoJSON('csvexample.csv', 'jsontestexample.json', true);
-    convert.JSONtoCSV('jsonexample.json', 'csvtestexample.csv');
+    convert.CSVtoJSON('./examples/csvexample.csv', './examples/jsontestexample.json', true);
+    convert.JSONtoCSV('./examples/jsonexample.json', './examples/csvtestexample.csv');
 
-    var originalcsv = fs.readFileSync('csvexample.csv');
+    var originalcsv = fs.readFileSync('./examples/csvexample.csv');
     originalcsv = originalcsv.toString();
 
-    var newcsv = fs.readFileSync('csvtestexample.csv');
+    var newcsv = fs.readFileSync('./examples/csvtestexample.csv');
     newcsv = newcsv.toString();
 
-    var originaljson = fs.readFileSync('jsonexample.json');
+    var originaljson = fs.readFileSync('./examples/jsonexample.json');
     originaljson = originaljson.toString();
 
-    var newjson = fs.readFileSync('jsontestexample.json');
+    var newjson = fs.readFileSync('./examples/jsontestexample.json');
     newjson = newjson.toString();
     it('converted csv files should be equal to what the correct file is', function(){
         assert(originalcsv.length == newcsv.length);
